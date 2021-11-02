6222610f50775d92d88b9643132f37724ebce48d
Hydra <41239639+HalfHydra@users.noreply.github.com>
2021-11-02 19:45:26 -0400

    Driver list finished, many images added.

--- Script/driverlist.js
+++ Script/driverlist.js
@@ -5,9 +5,206 @@ function generateDriverList(){
     Object.keys(savedata.Drivers).forEach((t,i)=>{
         let driverDiv = document.createElement('div');
         driverDiv.id = `driver_${t}`;
-        driverDiv.class = "driverDiv";
+        driverDiv.className = "driverDiv";
         output.appendChild(driverDiv);
 
+        //generateFaceEntry
+        let faceEntry = document.createElement('img');
+        faceEntry.className = "faceEntry";
+        faceEntry.src = `https://halfhydra.github.io/MarioKartTourValues/Images/FaceEntry/${values[savedata.Drivers[t].id].nameInternal}FaceEntry.png`;
+        switch(values[savedata.Drivers[t].id].rarityId){
+            case 0:
+                faceEntry.style.backgroundImage = `url('../Images/UI/Background/dnormal.png')`
+                break;
+            case 1:
+                faceEntry.style.backgroundImage = `url('../Images/UI/Background/drare.png')`
+                break;
+            case 2:
+                faceEntry.style.backgroundImage = `url('../Images/UI/Background/dhighend.png')`
+                break;
+        }
+        driverDiv.appendChild(faceEntry);
 
+        let driverTextDiv = document.createElement('div');
+        driverTextDiv.className = "driverTextDiv";
+        driverDiv.appendChild(driverTextDiv);
+
+        //generateName
+        let driverName = document.createElement('p');
+        driverName.className = "driverName";
+        driverName.innerHTML = `${values[savedata.Drivers[t].id].nameEng}`;
+        driverTextDiv.appendChild(driverName);
+
+        //generateDateReceived
+        let driverReceived = document.createElement('p');
+        driverReceived.className = "driverReceived";
+        console.log(savedata.Drivers[t].received_epoch);
+        driverReceived.innerHTML = `Received: ${new Date(savedata.Drivers[t].received_epoch * 1000).toLocaleString()}`;
+        driverTextDiv.appendChild(driverReceived);
+
+        //boxPoints
+        driverDiv.appendChild(generatePointsBox(0, savedata.Drivers[t].basepoints, savedata.Drivers[t].basepointsprogress, savedata.Drivers[t].pointCapLevel, values[savedata.Drivers[t].id].rarityId,t));
+
+        //boxLevel
+        driverDiv.appendChild(generateLevelBox(0,savedata.Drivers[t].level,0,t));
+
+        //boxCap
+        driverDiv.appendChild(generateCapLevel(0,values[savedata.Drivers[t].id].rarityId,savedata.Drivers[t].pointCapLevel));
     })
+}
+
+function generatePointsBox(itemType, points, progress, capLevel, rarity, index){
+    let boxPoints = document.createElement('div')
+    boxPoints.className = "boxPoints";
+    if(points == 1400 && rarity == 2 || points == 975 && rarity == 1 || points == 760 && rarity == 0){
+        boxPoints.style.backgroundImage = "url(./Images/UI/Lists/DriverMax.png)";
+    } else {
+        boxPoints.style.backgroundImage = "url(./Images/UI/Lists/DriverProgress.png)";
+    }
+
+    switch(rarity){
+        case 0:
+            if(points == 600 && capLevel < 1){
+                boxPoints.style.backgroundImage = "url(./Images/UI/Lists/DriverLocked.png)";
+            }
+            if(points == 648 && capLevel < 2){
+                boxPoints.style.backgroundImage = "url(./Images/UI/Lists/DriverLocked.png)";
+            }
+            if(points == 704 && capLevel < 3){
+                boxPoints.style.backgroundImage = "url(./Images/UI/Lists/DriverLocked.png)";
+            }
+            break;
+        case 1:
+            if(points == 675 && capLevel < 1){
+                boxPoints.style.backgroundImage = "url(./Images/UI/Lists/DriverLocked.png)";
+            }
+            if(points == 765 && capLevel < 2){
+                boxPoints.style.backgroundImage = "url(./Images/UI/Lists/DriverLocked.png)";
+            }
+            if(points == 870 && capLevel < 3){
+                boxPoints.style.backgroundImage = "url(./Images/UI/Lists/DriverLocked.png)";
+            }
+            break;
+        case 2:
+            if(points == 800 && capLevel < 1){
+                boxPoints.style.backgroundImage = "url(./Images/UI/Lists/DriverLocked.png)";
+            }
+            if(points == 980 && capLevel < 2){
+                boxPoints.style.backgroundImage = "url(./Images/UI/Lists/DriverLocked.png)";
+            }
+            if(points == 11190 && capLevel < 3){
+                boxPoints.style.backgroundImage = "url(./Images/UI/Lists/DriverLocked.png)";
+            }
+            break;
+    }
+
+    let pointsCount = document.createElement('div');
+    pointsCount.id = `pointsCount_${points}`;
+    pointsCount.className = "pointsContainer";
+
+    var charoutput = [];
+    for (var i = 0; i < points.toLocaleString().length; i++) {
+        charoutput.push(points.toLocaleString().charAt(i));
+    }
+    charoutput.forEach((t,i)=>{
+      var number = document.createElement('img');
+      number.className = `scoreNumber`;
+      if(t == ","){
+        number.className = `scoreComma`;
+      }
+      number.src = `./Images/UI/Number/${t}.png`
+      pointsCount.appendChild(number);
+    });
+    boxPoints.appendChild(pointsCount);
+
+    let converted = calcPoints(savedata.Drivers[`${index}`].total_xp, rarity, 0);
+    console.log(converted)
+
+    if(boxPoints.style.backgroundImage.includes('DriverProgress')){
+            let listlevelpanel = document.createElement('div');
+            listlevelpanel.id = `listpointpanel`
+            listlevelpanel.className = `listpointpanel`;
+            boxPoints.appendChild(listlevelpanel);
+        
+            var listlevelprogress = document.createElement('progress');
+            listlevelprogress.id = `listpointprogress`
+            listlevelprogress.className = 'listpointprogress';
+            listlevelprogress.src = "./Images/UI/specificitem1.png";
+            listlevelprogress.value = savedata.Drivers[`${index}`].basepointsprogress;
+            listlevelprogress.max = savedata.Drivers[`${index}`].basepointsrangetotal;
+            listlevelpanel.appendChild(listlevelprogress);
+                    
+            let listlevelprogresstext = document.createElement('p');
+            listlevelprogresstext.id = `listpointprogresstext`
+            listlevelprogresstext.className = `listpointprogresstext`;
+            listlevelprogresstext.innerHTML = `${savedata.Drivers[`${index}`].basepointsprogress} / ${savedata.Drivers[`${index}`].basepointsrangetotal}`;
+            listlevelpanel.appendChild(listlevelprogresstext);
+    }
+    return boxPoints;
+}
+
+function generateLevelBox(itemType, level, progress, index){
+    let boxLevel = document.createElement('div')
+    boxLevel.className = "boxLevel";
+    if(level == 7){
+        boxLevel.style.backgroundImage = "url(./Images/UI/Lists/DriverLevelMaxed.png)";
+    } else {
+        boxLevel.style.backgroundImage = "url(./Images/UI/Lists/DriverLevelProgress.png)";
+    }
+
+    var number = document.createElement('img');
+    number.className = `levelNumber`;
+    number.src = `./Images/UI/LevelNumber/${level}.png`
+    boxLevel.appendChild(number);
+
+    let converted = convertToLevel(savedata.Drivers[`${index}`].totalCount, values[`${savedata.Drivers[`${index}`].id}`].rarityId);
+    console.log(converted)
+
+    if(level != 7){
+    let listlevelpanel = document.createElement('div');
+    listlevelpanel.id = `listlevelpanel`
+    listlevelpanel.className = `listlevelpanel`;
+    boxLevel.appendChild(listlevelpanel);
+
+    var listlevelprogress = document.createElement('progress');
+    listlevelprogress.id = `listlevelprogress`
+    listlevelprogress.className = 'listlevelprogress';
+    listlevelprogress.src = "./Images/UI/specificitem1.png";
+    listlevelprogress.value = converted[1];
+    listlevelprogress.max = converted[2];
+    listlevelpanel.appendChild(listlevelprogress);
+            
+    let listlevelprogresstext = document.createElement('p');
+    listlevelprogresstext.id = `listlevelprogresstext`
+    listlevelprogresstext.className = `listlevelprogresstext`;
+    listlevelprogresstext.innerHTML = `${converted[1]} / ${converted[2]}`;
+    listlevelpanel.appendChild(listlevelprogresstext);
+    }
+    return boxLevel;
+}
+
+function generateCapLevel(itemType, rarity, level){
+    let boxCap = document.createElement('div');
+    boxCap.className = "boxCap";
+
+    let ticketImg = document.createElement('img')
+    ticketImg.className = "ticketImg"
+    switch(rarity){
+        case 0:
+            ticketImg.src = "./Images/UI/Tickets/TicketScoreUpUnlock_Driver_Normal.png"
+            break;
+        case 1:
+            ticketImg.src = "./Images/UI/Tickets/TicketScoreUpUnlock_Driver_Rare.png"
+            break;
+        case 2:
+            ticketImg.src = "./Images/UI/Tickets/TicketScoreUpUnlock_Driver_Ultra.png"
+            break;
+    }
+    boxCap.appendChild(ticketImg);
+
+    let capNum = document.createElement('p');
+    capNum.innerHTML = `x${level}`;
+    capNum.className = "capNum";
+    boxCap.appendChild(capNum);
+    return boxCap;
 }
--- Script/global.js
+++ Script/global.js
@@ -37,6 +37,14 @@ let driverTable = [];
 let machineTable = [];
 let wingTable = [];
 
+    let driverN = { "0": 400, "8": 408, "17": 416, "27": 424, "38": 432, "50": 440, "63": 448, "77": 456, "92": 464, "108": 472, "126": 480, "146": 488, "168": 496, "192": 504, "218": 512, "246": 520, "276": 528, "308": 536, "342": 544, "378": 552, "417": 560, "459": 568, "504": 576, "552": 584, "603": 592, "657": 600, "714": 608, "772": 616, "831": 624, "891": 632, "952": 640, "1014": 648, "1077": 656, "1141": 664, "1206": 672, "1272": 680, "1339": 688, "1407": 696, "1476": 704, "1546": 712, "1617": 720, "1689": 728, "1762": 736, "1836": 744, "1911": 752, "1987": 760 };
+    let driverS = { "0": 450, "8": 459, "17": 468, "27": 477, "38": 486, "50": 495, "63": 504, "77": 513, "92": 522, "108": 531, "126": 540, "146": 549, "168": 558, "192": 567, "218": 576, "246": 585, "276": 594, "308": 603, "342": 612, "378": 621, "417": 630, "459": 639, "504": 648, "552": 657, "603": 666, "657": 675, "714": 690, "772": 705, "831": 720, "891": 735, "952": 750, "1014": 765, "1077": 780, "1141": 795, "1206": 810, "1272": 825, "1339": 840, "1407": 855, "1476": 870, "1546": 885, "1617": 900, "1689": 915, "1762": 930, "1836": 945, "1911": 960, "1987": 975 };
+    let driverH = { "0": 500, "8": 512, "17": 524, "27": 536, "38": 548, "50": 560, "63": 572, "77": 584, "92": 596, "108": 608, "126": 620, "146": 632, "168": 644, "192": 656, "218": 668, "246": 680, "276": 692, "308": 704, "342": 716, "378": 728, "417": 740, "459": 752, "504": 764, "552": 776, "603": 788, "657": 800, "714": 830, "772": 860, "831": 890, "891": 920, "952": 950, "1014": 980, "1077": 1010, "1141": 1040, "1206": 1070, "1272": 1100, "1339": 1130, "1407": 1160, "1476": 1190, "1546": 1220, "1617": 1250, "1689": 1280, "1762": 1310, "1836": 1340, "1911": 1370, "1987": 1400 };
+    let kartsN = { "0": 200, "6": 204, "13": 208, "21": 212, "30": 216, "40": 220, "51": 224, "63": 228, "76": 232, "90": 236, "105": 240, "121": 244, "138": 248, "156": 252, "175": 256, "195": 260, "216": 264, "238": 268, "261": 272, "285": 276, "311": 280, "339": 284, "369": 288, "401": 292, "435": 296, "471": 300, "509": 304, "548": 308, "588": 312, "629": 316, "671": 320, "714": 324, "758": 328, "803": 332, "849": 336, "896": 340, "944": 344, "993": 348, "1043": 352, "1094": 356, "1146": 360, "1199": 364, "1253": 368, "1308": 372, "1364": 376, "1421": 380 };
+    let kartsS = { "0": 220, "6": 224, "13": 228, "21": 232, "30": 236, "40": 240, "51": 244, "63": 248, "76": 252, "90": 256, "105": 260, "121": 264, "138": 268, "156": 272, "175": 276, "195": 280, "216": 285, "238": 290, "261": 295, "285": 300, "311": 305, "339": 310, "369": 315, "401": 320, "435": 325, "471": 330, "509": 336, "548": 342, "588": 348, "629": 354, "671": 360, "714": 366, "758": 372, "803": 378, "849": 384, "896": 390, "944": 396, "993": 402, "1043": 408, "1094": 414, "1146": 420, "1199": 426, "1253": 432, "1308": 438, "1364": 444, "1421": 450 };
+    let kartsH = { "0": 250, "6": 256, "13": 262, "21": 268, "30": 274, "40": 280, "51": 286, "63": 292, "76": 298, "90": 304, "105": 310, "121": 316, "138": 322, "156": 328, "175": 334, "195": 340, "216": 346, "238": 352, "261": 358, "285": 364, "311": 370, "339": 376, "369": 382, "401": 388, "435": 394, "471": 400, "509": 415, "548": 430, "588": 445, "629": 460, "671": 475, "714": 490, "758": 505, "803": 520, "849": 535, "896": 550, "944": 565, "993": 580, "1043": 595, "1094": 610, "1146": 625, "1199": 640, "1253": 655, "1308": 670, "1364": 685, "1421": 700 };
+
+
 function generateArrays(){
 
     Object.keys(values).forEach(itemId => {
@@ -210,4 +218,118 @@ function hideAllTabsButOne(tab){
     document.getElementById('badges').style.display = "none";
     document.getElementById('items').style.display = "none";
     document.getElementById(tab).style.display = "block";
+}
+
+function convertToLevel(input, rarity){
+    let level = 0;
+    let remainder = input;
+    let levels = {"2":[1,1,1,2,2,3,5],"1":[1,1,2,3,4,5,8],"0":[1,2,5,8,11,14,20]};
+    let required = 0;
+    while(remainder - levels[rarity][level] >= 0){
+      remainder -= levels[rarity][level];
+      level++;
+    }
+    required = levels[rarity][level];
+    return [level, remainder, required];
+}
+
+function calcPoints(input, rarity, type) {
+    //console.log(`input = ${input}, rarity = ${rarity}, type = ${type}`);
+    let points = 0;
+    let iterate = 0;
+    let remainder = input;
+    let amounts = [];
+    switch (rarity) {
+        case 0:
+            switch (type) {
+                case 0:
+                    Object.keys(driverN).forEach((k, i) => {
+                        amounts.push(parseInt(k));
+                    });
+                    break;
+                case 1:
+                    Object.keys(kartsN).forEach((k, i) => {
+                        amounts.push(parseInt(k));
+                    });
+                    break;
+            }
+            break;
+        case 1:
+            switch (type) {
+                case 0:
+                    Object.keys(driverS).forEach((k, i) => {
+                        amounts.push(parseInt(k));
+                    });
+                    break;
+                case 1:
+                    Object.keys(kartsS).forEach((k, i) => {
+                        amounts.push(parseInt(k));
+                    });
+                    break;
+            }
+            break;
+        case 2:
+            switch (type) {
+                case 0:
+                    Object.keys(driverH).forEach((k, i) => {
+                        amounts.push(parseInt(k));
+                    });
+                    break;
+                case 1:
+                    Object.keys(kartsH).forEach((k, i) => {
+                        amounts.push(parseInt(k));
+                    });
+                    break;
+            }
+            break;
+    }
+    let amountItr = 0;
+    while ((remainder - amounts[amountItr]) > 0) {
+        amountItr++;
+    }
+    points = amounts[amountItr - 1];
+    remainder = input - amounts[amountItr - 1];
+    if (input == amounts[amounts.length - 1]) {
+        points = amounts[amounts.length - 1];
+        remainder = 0;
+    }
+    if (input == amounts[31]) {
+        points = amounts[31];
+        remainder = 0;
+    }
+    if (input == amounts[38]) {
+        points = amounts[38];
+        remainder = 0;
+    }
+    if (input == 0) {
+        points = 0;
+        remainder = 0;
+    }
+    switch (rarity) {
+        case 0:
+            switch (type) {
+                case 0:
+                    return [`${points}`, remainder, amounts[amountItr] - points];
+                case 1:
+                    return [`${points}`, remainder, amounts[amountItr] - points];
+            }
+            break;
+        case 1:
+            switch (type) {
+                case 0:
+                    return [`${points}`, remainder, amounts[amountItr] - points];
+                case 1:
+                    return [`${points}`, remainder, amounts[amountItr] - points];
+            }
+            break;
+        case 2:
+            switch (type) {
+                case 0:
+                    return [`${points}`, remainder, amounts[amountItr] - points];
+                case 1:
+                    return [`${points}`, remainder, amounts[amountItr] - points];
+            }
+            break;
+    }
+    return [points, remainder];
 }
